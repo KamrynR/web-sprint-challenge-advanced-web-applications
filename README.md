55cd8093e28d7858e3497df647a49ce3268ce46b
KamrynR <62856359+KamrynR@users.noreply.github.com>
2020-09-11 21:03:24 -0500

    1

--- README.md
+++ README.md
@@ -28,9 +28,13 @@ Commit your code regularly and meaningfully. This helps both you (in case you ev
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. You might prepare by writing down your own answers before hand.
 
 1. Explain what a token is used for.
+A token is for authenticating a user for access to a specific component/data sets
 2. What steps can you take in your web apps to keep your data secure?
+Private routes and securing API keys
 3. Describe how web servers work.
+Servers give data upon a HTTP request
 4. Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+Create/post, read/get, update/put, delete/delete
 
 You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade. 
 
--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,26 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, getByText, render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import App from '../App';
 
 test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+  // const {getByTestId} = render(<BubblePage />);
+  // const bubbles = getByTestId(/bubble/i);
+  // expect(bubbles).toHaveLength(11);
+
+  // const {getByTestId, rerender} = render(<App/>)
+  // const username = getByTestId(/username/i)
+  // const password = getByTestId(/password/i)
+  // fireEvent.change(username, { target: { value: 'Lambda School' } })
+  // fireEvent.change(password, { target: { value: 'i<3Lambd4' } })
+  // expect(username.value).toBe('Lambda School')
+  // expect(password.value).toBe('i<3Lambd4')
+  // const leftClick = { button: 1 }
+  // const button = getByTestId(/loginbtn/i)
+  // fireEvent.click(button, leftClick)
+  // const button = getByTestId(/loginbtn/i)
+  // fireEvent.click(button)
+  const {getByTestId} = render(<BubblePage />)
+  const bubbles = getByTestId('/bubble/i')
+  expect(bubbles).toHaveLength(11)
 });
